371d827a8f946075d007bc1adc5a01e425b62ebd
任捷 <murlocs790608@gmail.com>
2019-06-27 20:19:51 +0800

    first commit

---
+++ PHigh/src/main/java/tw/org/iii/eeit107/team04/PHighApplication.java
@@ -0,0 +1,13 @@
+package tw.org.iii.eeit107.team04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PHighApplication.class, args);
+	}
+
+}
